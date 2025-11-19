2825a369d392b3b36ac74dc2182f301e33710c05
Maik Laschober <maik.laschober@omega-center.com>
2025-11-19 21:02:43 +0100

    refactor: entrypoint.sh (remove loop, causing bugs)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,14 @@ GAME_ID=339010
 mkdir -p ${GAME_DIR}
 chown -R steam:steam ${GAME_DIR}
 
-while true; do
-    echo "-------------------------------START------------------------------"
-    su -c "~/steamcmd/steamcmd.sh +force_install_dir ${GAME_DIR} +login anonymous +app_update ${GAME_ID} validate +quit" steam
+echo "-------------------------------START------------------------------"
+su -c "~/steamcmd/steamcmd.sh +force_install_dir ${GAME_DIR} +login anonymous +app_update ${GAME_ID} validate +quit" steam
 
-    cd ${GAME_DIR}
-    export LD_LIBRARY_PATH="${GAME_DIR}/linux64:${GAME_DIR}:$LD_LIBRARY_PATH"
+cd ${GAME_DIR}
+export LD_LIBRARY_PATH="${GAME_DIR}/linux64:${GAME_DIR}:$LD_LIBRARY_PATH"
 
-    ./RisingWorldServer.x64
+./RisingWorldServer.x64
 
-    echo "--------------------------------END-------------------------------"
-    echo "Server exited, restarting in 5 seconds..."
-    sleep 5
-done
+echo "--------------------------------END-------------------------------"
+echo "Server exited, restarting in 5 seconds..."
+sleep 5
